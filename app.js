5d61e8484a245052ad700996ef5624ed33f59d6f
Sanket Kumar <sanketcharanpahadi@gmail.com>
2022-12-11 22:19:16 +0530

    final commit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const notFound = require("./middleware/notFound");
 const userRouter = require("./routes/userRoute");
 const cors = require("cors");
 const chatRouter = require("./routes/chatRoute");
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
@@ -22,6 +23,23 @@ mongoose
 
 app.use("/api/users", userRouter);
 app.use("/api/chat", chatRouter);
+
+// Deployment
+
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running.");
+  });
+}
+// Deployment
+
 app.use(notFound);
 app.use(errorHandler);
 
